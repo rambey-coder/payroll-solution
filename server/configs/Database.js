9ab381c75a8fa88b21d7657c4ce41e7da62e6f6a
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-07-22 12:28:00 +0100

    merging

--- server/configs/Database.js
+++ server/configs/Database.js
@@ -2,16 +2,16 @@ import { Sequelize } from 'sequelize';
 
 
 const db = new Sequelize(
-  // 'infonom1_payroll',
-  // 'infonom1_infonom',
-  // 'infonomicsng',
-  'info_payroll',
-  'root',
-  'Ajibolas7',
+  'infonom1_payroll',
+  'infonom1_infonom',
+  'infonomicsng',
+  // 'info_payroll',
+  // 'root',
+  // 'Ajibolas7',
   {
     dialect: 'mysql',
-    // host: '176.74.18.130',
-    host: '127.0.0.1',
+    host: '176.74.18.130',
+    // host: '127.0.0.1',
     port: 3306,
     logging: false
     // host: process.env.DB_HOST,
