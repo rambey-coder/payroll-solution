dab39eb95b3ff9a0193dcf55a3f81c2d6be45fbf
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-07-22 17:53:53 +0100

    committing db change

--- server/configs/Database.js
+++ server/configs/Database.js
@@ -2,18 +2,18 @@ import { Sequelize } from 'sequelize';
 
 
 const db = new Sequelize(
-  // 'infonom1_payroll',
-  // 'infonom1_infonom',
-  // 'infonomicsng',
-  'info_payroll',
-  'root',
-  'Ajibolas7',
+  'infonom1_payroll',
+  'infonom1_infonom',
+  'infonomicsng',
+  // 'info_payroll',
+  // 'root',
+  // 'Ajibolas7',
   {
     dialect: 'mysql',
-    // host: '176.74.18.130',
-    host: '127.0.0.1',
+    host: '176.74.18.130',
+    // host: '127.0.0.1',
     port: 3306,
-    logging: false
+    logging: true
     // host: process.env.DB_HOST,
     // port: process.env.DB_PORT,
   }
